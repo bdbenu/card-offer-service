d9869db3bafb38700dceabc377669c14c5c1f3c0
bdbenu <bdbenu18@gmail.com>
2023-06-04 13:17:13 +0530

    minor modification

--- card-offer-service/src/main/java/com/keybank/offers/CardOfferServiceApplication.java
+++ card-offer-service/src/main/java/com/keybank/offers/CardOfferServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CardOfferServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CardOfferServiceApplication.class, args);
+		SpringApplication.run(CardOfferServiceApplication.class, args); 
 	}
 
 }
--- card-offer-service/src/main/java/com/keybank/offers/model/OffersDaoRequest.java
+++ card-offer-service/src/main/java/com/keybank/offers/model/OffersDaoRequest.java
@@ -10,5 +10,5 @@ package com.keybank.offers.model;
  *
  */
 public class OffersDaoRequest {
-
+  
 }
